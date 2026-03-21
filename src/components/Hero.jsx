2c3672108fdcd7bb88161d2a138c30d8d9d620a7
Shubham Kulkarni <shubhamdadakulkarni13@gmail.com>
2026-03-22 00:34:05 +0530

    Hero section updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,6 +10,17 @@ function Hero() {
         <h1>Guru Global School 🌟</h1>
         <p style={{ fontSize: "18px" }}>We Care, We Create</p>
         <p><b>Admissions Open 2026–27</b></p>
+        <p style={{
+          background: "white",
+          color: "#333",
+          display: "inline-block",
+          padding: "5px 12px",
+          borderRadius: "20px",
+          fontSize: "12px",
+          marginTop: "10px"
+          }}>
+            ISO 9001:2015 Certified
+            </p>
 
         <a href={WHATSAPP_LINK} className="btn">
           Enroll via WhatsApp
